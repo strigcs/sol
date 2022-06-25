5cc03534b3f65b2c1c58bff4a9cc1cd4874cc8e3
strigcs <strigcs@gmail.com>
2022-06-25 22:02:39 +0300

    Add files via upload

--- Inbox.test.js
+++ Inbox.test.js
@@ -3,16 +3,21 @@ const ganache = require('ganache-cli');
 const { describe } = require('mocha');
 const Web3 = require('web3')
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode} = require('../compile');
 
-beforeEach (() => {
-    web3.eth.getAccounts()
-        .then(fetchedAccounts => {
-            console.log(fetchedAccounts)
-        });
+let accounts;
+let inbox;
 
+beforeEach (async () => {
+    accounts = await web3.eth.getAccounts();
+
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there!'] })
+        .send({ from: accounts[0], gas: '1000000'})
 });
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
+        console.log(inbox)
     });
 });

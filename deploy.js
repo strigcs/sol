a2fc853ad8091b374b4fedb3d5d7189a51d2bf9b
strigcs <strigcs@gmail.com>
2022-07-25 08:26:23 +0300

    apdated

--- deploy.js
+++ deploy.js
@@ -1,23 +1,25 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
-const { interface, bytecode } = require('./compile');
 
-const provider = new HDWalletProvider(
-    'sudden day motor slush logic wool tornado improve devote human error list',
-    'https://rinkeby.infura.io/v3/5cb14c57234a411b80ad5edc00916d50'
+const { abi, evm } = require('./compile');
+
+provider = new HDWalletProvider(
+  'REPLACE_WITH_YOUR_MNEMONIC',
+  'REPLACE_WITH_YOUR_INFURA_URL'
 );
+
 const web3 = new Web3(provider);
 
-const deploy = async() => {
-    const accounts = await web3.eth.getAccounts();
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
 
-    console.log('Attemting to deploy from account', accounts[0])
+  console.log('Attempting to deploy from account', accounts[0]);
 
-    const result = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: ['Hi there!'] })
-        .send({ gas: '1000000', from: accounts[0] });
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
 
-    console.log('Contract deployed to', result.options.address);
-    provider.engine.stop()
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
 };
 deploy();
